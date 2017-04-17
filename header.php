48024f014dea5112cc4ec9a044b005e05edff542
vladloza <vladok001@gmail.com>
2017-04-17 20:28:47 +0300

    create

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php?><!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src='js/jquery-3.1.1.min.js'></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body class="home blog custom-font-enabled single-author hfeed">
+<div id="page">
+	<header id="masthead" class="site-header container" role="banner">
+      <div class="navbar" role="navigation" style="margin:0px;">
+      <div>
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse themonic-nav">   
+          <ul class="nav-menu">
+            <li class="active"><a href="#">Головна</a></li>
+            <li><a href="#about">Про кафедру</a></li>
+            <li><a href="#contact">Абітурієнту</a></li>
+            <li><a href="#contact">Співробітники</a></li>
+            <li><a href="#contact">Студентам</a></li>
+            <li><a href="#contact">Новини</a></li>
+            <li><a href="#contact">Послуги</a></li>
+            <li><a href="#contact">Контакти</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+        <div class="clear"></div>
+      </div>
+      </header>	
+
+
+
---
+++ news.php
@@ -0,0 +1,49 @@
+<?php include("header.php") ?>
+<div id="main" class="container margin-top">
+    <div class="wrapper2">
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
+            <article>
+                <header class="entry-header">
+                    <h2 class="entry-title">
+                        <a href="#">Elementss</a>
+                    </h2>
+                </header>
+                <div class="entry-summary">
+                    <p>The purpose of this HTML is to help determine what default settings are with CSS and to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible Elements when designing a site. Heading 1 Heading 2 Heading 3 Heading 4 Heading 5 Heading 6 [top] Paragraph… 
+                        <span class="read-more">
+                            <a href="#">Read more</a>
+                        </span>
+                    </p>   
+                </div>
+                <footer class="entry-meta"></footer>
+            </article>
+             <article>
+                <header class="entry-header">
+                    <h2 class="entry-title">
+                        <a href="#">Elementss</a>
+                    </h2>
+                </header>
+                <div class="entry-summary">
+                    <p>The purpose of this HTML is to help determine what default settings are with CSS and to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible Elements when designing a site. Heading 1 Heading 2 Heading 3 Heading 4 Heading 5 Heading 6 [top] Paragraph… 
+                        <span class="read-more">
+                            <a href="#">Read more</a>
+                        </span>
+                    </p>   
+                </div>
+                <footer class="entry-meta"></footer>
+            </article>
+        </div>
+    </div>
+    <div class="pager">
+        <a href="#" class="active">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+    </div>
+</div>
+	</div><!-- #main .wrapper -->
+       <?php include("footer.php") ?>
+        <div class="clear"></div>
+</div><!-- #page -->
+</body>
+</html>
