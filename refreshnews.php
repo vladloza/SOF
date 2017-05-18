7f4e3c31a7dcc6fc0d9e51304cc8635f5a3fe2e7
vladloza <vladok001@gmail.com>
2017-05-18 12:28:21 +0300

    fix news

--- refreshnews.php
+++ refreshnews.php
@@ -1,6 +1,7 @@
 <?php
 include 'dbconfig.php';
-
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 $startFrom = $_POST['startFrom'];
 
 $sqlQuery = 'select * from News order by id desc limit '.$startFrom.', 5';
@@ -27,6 +28,14 @@ while($row = $result->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT))
     $article[3] = $row[3];
     $article[4] = $row[4];
     $article[5] = $row[5];
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    if(isset($_SESSION['isLogged']))
+    {
+        $article[6] = $_SESSION['isLogged'];
+    }
     $articles[] = $article;
 }
 echo json_encode($articles);
